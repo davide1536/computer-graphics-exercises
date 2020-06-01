229fe3d3dbbe6909bcdf00e56ff5ee61a199e804
Xavija <lohmen@protonmail.com>
2020-06-01 23:11:31 +0200

    solarSystem.c: ora compila, da sistemare illuminazione

--- 4/solarSystem.c
+++ 4/solarSystem.c
@@ -6,15 +6,27 @@
 void rotatePlanetAngle(int);
 void loopPlanets(void);
 void initPlanets(void);
-void initLights(void);
 void init(void);
 void drawPlanets(void);
 void display(void);
 int  main(int, char**);
 
+/* Uncomment to enable lighting. */
+//#define LIGHTING
+
+/* Uncomment to enable planet orbit animations. */
+#define ANIMATION
+
 /* Total number of planets in the scene. */
 #define PLANETS_MAX	9
 const float DEG2RAD = 3.14159/180.0;
+
+typedef struct _planet_material_t {
+	float	matAmbient[4],
+			matDiffuse[4],
+			matSpecular[4],
+			shine[1];
+} PlanetMaterial;
 
 /* struct Planet: represents planets info, such as:
  * - radius
@@ -25,20 +37,20 @@ const float DEG2RAD = 3.14159/180.0;
  * - 
  */
 typedef struct _planet_t {
-	GLdouble radius;
-	GLdouble distance;
-	GLdouble r, 
-			 g, 
-			 b;
-	GLdouble x,
-			 y;
-	GLdouble xMaxOrbit,
-			 yMaxOrbit;
-	int      speed,
-			 currentAngle;
+	GLdouble        radius;
+	GLdouble        distance;
+	GLdouble        x,
+					y;
+	GLdouble        xMaxOrbit,
+					yMaxOrbit;
+	GLdouble		r,
+			 		g,
+			 		b;
+	PlanetMaterial 	material;
+	int             speed,
+					currentAngle;
 } Planet;
 
-=======
 /* Initial coordinates of the planets (x, 0.0, 0.0). */
 GLdouble planetCoordinates[PLANETS_MAX] = {
 	0.0,                /* Sun */
@@ -79,8 +91,8 @@ GLdouble planetRadiuses[PLANETS_MAX] = {
 };
 
 /* Orbit coordinates. 
-* TODO redundancy: x coord already referenced by planet's inital x parameter
-*/
+ * TODO redundancy: x coord already referenced by planet's inital x parameter
+ */
 GLdouble planetMaxOrbit[PLANETS_MAX][2] = {
 	{0.0, 0.0},			/* Sun */
 	{7.0, 5.0},			/* Mercury */
@@ -92,10 +104,28 @@ GLdouble planetMaxOrbit[PLANETS_MAX][2] = {
 	{45.5, 30.0},		/* Uranus */
 	{53.6, 48.0},		/* Neptune */
 };
->>>>>>> a00a503601f838b4f8933985b99216e8683a0d2d
+
+/* Materials (lighting). */
+PlanetMaterial planetMaterials[PLANETS_MAX] = {
+	{{0.5,0.0,1.0},{1.0,1.0,0.0,1.0},{0,0,0,1.0},{20.0}},
+	{{0.6,0.3,0.0,1.0},{0.4,0.3,0.5,1.0},{0,0,0,1.0},{20.6}},
+
+	{{0.74,0.59,0.074,1.0},{0.35,0.20,0.22,1.0},{0.0,0.0,0.0,1.0},{20.0}},
+
+	{{0.5,0.5,0.5,1.0},{0.5,0.5,0.5,1.0},{0,0,0,1.0},{20.0}},
+
+	{{0.5,0.5,0.5,1.0},{0.5,0.5,0.5,1.0},{0,0,0,1.0},{20.0}},
+
+	{{0.5,0.5,0.5,1.0},{0.5,0.5,0.5,1.0},{0,0,0,1.0},{20.0}},
+
+	{{0.5,0.5,0.5,1.0},{0.5,0.5,0.5,1.0},{0,0,0,1.0},{20.0}},
+
+	{{0.5,0.5,0.5,1.0},{0.5,0.5,0.5,1.0},{0,0,0,1.0},{20.0}},
+	
+	{{0.5,0.5,0.5,1.0},{0.5,0.5,0.5,1.0},{0,0,0,1.0},{20.0}}
+};
 
 static Planet planets[PLANETS_MAX];
-planetMaterial planetsMaterials[PLANETS_MAX-1];
 static int angle = 0;
 
 void rotatePlanetAngle(int val) {
@@ -108,10 +138,10 @@ void rotatePlanetAngle(int val) {
  * in order to perform an elliptical orbit
  * around the sun.
  */
-void loopPlanets() {
+/* void loopPlanets() {
 	float degInRad;
 	for(int i = 1; i < PLANETS_MAX; i++) {
-		/* Draw orbital trails */
+		// Draw orbital trails
 		glBegin(GL_POINTS);
 			glColor3f(0.8, 0.8, 0.8);
 			for(int j = 0; j < 360; j++) {
@@ -131,7 +161,7 @@ void loopPlanets() {
 		glPopMatrix();
 	}
 	
-}
+} */
 
 void drawPlanets() {
 	GLUquadric *quad;
@@ -140,44 +170,63 @@ void drawPlanets() {
 	for(int i = 0; i < PLANETS_MAX; i++) {
 	
 		glPushMatrix();
-		// color
+		/* Colors */
 		glColor3f(planets[i].r, planets[i].g, planets[i].b);
 		
-		// angle
+		/* Angle */
 		glRotatef((GLfloat) angle, 0.0, 1.0, 0.0);   
 		
-
-		// position
+		/* Position */
 		glTranslatef(planets[i].x, planets[i].y, 0.0);
+
+		/* Assign a material to each planet */
+		#ifdef LIGHTING
+			glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT,   planets[i].material.matAmbient);
+			glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE,   planets[i].material.matDiffuse);
+			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR,  planets[i].material.matSpecular);
+			glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, planets[i].material.shine);
+			/* glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, planetMaterials[i].matAmbient);
+			glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, planetMaterials[i].matDiffuse);
+			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, planetMaterials[i].matSpecular);
+			glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS,planetMaterials[i].shine); */
+		#endif
 		
-		// sphere
+
+		/* Draw a sphere */
 		glutWireSphere(planets[i].radius, 20, 20);
 
 		glPopMatrix();
 
 		// draw orbit trail
-		glBegin(GL_POINTS);
+		/* glBegin(GL_POINTS);
 			glColor3f(0.8, 0.8, 0.8);
 			for(int j = 0; j < 360; j++) {
 				float degInRad = j*DEG2RAD;
 				glVertex3f(cos(degInRad)*planets[i].xMaxOrbit, sin(degInRad)*planets[i].yMaxOrbit, 0.0);
 			}
-		glEnd();
-		/* glPushMatrix();
+		glEnd(); */
+
+		// No, that won't work.
+		glPushMatrix();
 		glTranslatef(0.0, 0.0, 0.0);
 		glRotatef(90.0, 1.0, 0.0, 0.0);
 		glColor3f(0.8, 0.8, 0.8);
 		glutWireTorus(.001, planets[i].xMaxOrbit, 100.0, 100.0);
-		glPopMatrix(); */
+		glPopMatrix();
 	}
 }
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+	GLfloat lightPos[4] = {0};
+
 	/* Push initial modelview matrix */
 	glPushMatrix();
 	
+	/* Initialize light position */
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+
 	/* Draw elements in the scene */
 	drawPlanets();
 
@@ -207,129 +256,9 @@ void initPlanets() {
 		planets[i].yMaxOrbit	= planetMaxOrbit[i][1];
 		/* Current angle */
 		planets[i].currentAngle	= 0;
+		/* Lighting materials */
+		planets[i].material 	= planetMaterials[i];
 	}
-/* 	// Sun
-	planets[0].r		= 1.0;
-	planets[0].g		= 1.0;
-	planets[0].b		= 0.0;
-	planets[0].radius	= 5.0;
-	planets[0].distance	= 0.0;
-	planets[0].x        = 0.0;
-	planets[0].y        = 0.0;
-
-	// Mercury
-	planets[1].r		= 0.5;
-	planets[1].g		= 0.3;
-	planets[1].b		= 0.0;
-	planets[1].radius	= 1.0;
-	planets[1].distance	= 7.0;
-	planets[1].x        = 7.0;
-	planets[1].y        = 0.0;
-
-	// Venus
-	planets[2].r		= 1.0;
-	planets[2].g		= 0.5;
-	planets[2].b		= 0.0;
-	planets[2].radius	= 1.5;
-	planets[2].distance	= 11.0;
-	planets[2].x        = 11.0;
-	planets[2].y        = 0.0;
-
-	// Earth
-	planets[3].r		= 0.0;
-	planets[3].g		= 0.0;
-	planets[3].b		= 1.0;
-	planets[3].radius	= 2.0;
-	planets[3].distance	= 16.0;
-	planets[3].x        = 16.0;
-	planets[3].y        = 0.0;
-
-	// Mars
-	planets[4].r		= 1.0;
-	planets[4].g		= 0.2;
-	planets[4].b		= 0.0;
-	planets[4].radius	= 1.2;
-	planets[4].distance	= 21.0;
-	planets[4].x        = 21.0;
-	planets[4].y        = 0.0;
-
-	// Jupiter
-	planets[5].r		= 1.0;
-	planets[5].g		= 0.5;
-	planets[5].b		= 0.0;
-	planets[5].radius	= 3.5;
-	planets[5].distance	= 28.0;
-	planets[5].x        = 28.0;
-	planets[5].y        = 0.0;
-
-	// Saturn
-	planets[6].r		= 1.0;
-	planets[6].g		= 0.7;
-	planets[6].b		= 0.0;
-	planets[6].radius	= 3.0;
-	planets[6].distance	= 37.0;
-	planets[6].x        = 37.0;
-	planets[6].y        = 0.0;
-
-	// Uranus
-	planets[7].r		= 0.7;
-	planets[7].g		= 0.7;
-	planets[7].b		= 1.0;
-	planets[7].radius	= 2.5;
-	planets[7].distance	= 45.5;
-	planets[7].x        = 45.5;
-	planets[7].y        = 0.0;
-
-	// Neptune
-	planets[8].r		= 0.3;
-	planets[8].g		= 0.3;
-	planets[8].b		= 1.0;
-	planets[8].radius	= 2.3;
-	planets[8].distance	= 53.6;
-	planets[8].x        = 53.6;
-	planets[8].y        = 0.0;
-	 */
-}
-	// //mercury
-	// planetsMaterials[1].mat_ambient[]	= {1.0};
-	// planetsMaterials[1].mat_diffuse[]	= {1.0};
-	// planetsMaterials[1].mat_specular[]	= {1.0};
-	// planetsMaterials[1].shine[]			= {1.0};
-	// //venus
-	// planetsMaterials[2].mat_ambient		= {1.0};
-	// planetsMaterials[2].mat_diffuse		= {1.0};
-	// planetsMaterials[2].mat_specular	= {1.0};
-	// planetsMaterials[2].shine			= {1.0};
-	// //earth
-	// planetsMaterials[3].mat_ambient		= {1.0};
-	// planetsMaterials[3].mat_diffuse		= {1.0};
-	// planetsMaterials[3].mat_specular	= {1.0};
-	// planetsMaterials[3].shine			= {1.0};
-	// //mars
-	// planetsMaterials[4].mat_ambient		= {1.0};
-	// planetsMaterials[4].mat_diffuse		= {1.0};
-	// planetsMaterials[4].mat_specular	= {1.0};
-	// planetsMaterials[4].shine			= {1.0};
-	// //jupiter
-	// planetsMaterials[5].mat_ambient		= {1.0};
-	// planetsMaterials[5].mat_diffuse		= {1.0};
-	// planetsMaterials[5].mat_specular	= {1.0};
-	// planetsMaterials[5].shine			= {1.0};
-	// //saturn
-	// planetsMaterials[6].mat_ambient		= {1.0};
-	// planetsMaterials[6].mat_diffuse		= {1.0};
-	// planetsMaterials[6].mat_specular	= {1.0};
-	// planetsMaterials[6].shine			= {1.0};
-	// //uranus
-	// planetsMaterials[7].mat_ambient		= {1.0};
-	// planetsMaterials[7].mat_diffuse		= {1.0};
-	// planetsMaterials[7].mat_specular	= {1.0};
-	// planetsMaterials[7].shine			= {1.0};
-	// //neptune
-	// planetsMaterials[8].mat_ambient		= {1.0};
-	// planetsMaterials[8].mat_diffuse		= {1.0};
-	// planetsMaterials[8].mat_specular	= {1.0};
-	// planetsMaterials[8].shine			= {1.0};
 }
 
 void init() {
@@ -338,29 +267,27 @@ void init() {
 	// initalize planet values
 	initPlanets();
 
-	initLights();
+	#ifdef LIGHTING
+		// Light init
+		float globAmb[] = {0.0, 0.0, 0.0, 1.0};
+		float lightAmb[] = {0.2, 0.2,0.2, 1.0};
+		float lightDif[] = {1.0, 1.0, 1.0, 1.0};
+		float lightSpec[] = {1.0, 1.0, 1.0, 1.0};
 
-	//define light
-	float globAmb[] = {0.0, 0.0, 0.0, 1.0};
+		glEnable(GL_LIGHTING);
+		
+		glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmb);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDif);
+		glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpec);
 
-    // Light vectors.
-    float lightAmb[] = {0.2, 0.2,0.2, 1.0};
-    float lightDif[] = {1.0, 1.0, 1.0, 1.0};
-    float lightSpec[] = {1.0, 1.0, 1.0, 1.0};
-	
-	glEnable(GL_LIGHTING);
+		// Global ambient light.
+		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globAmb);
 
-	glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmb);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDif);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpec);
-
-    // Global ambient light.
-    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, globAmb);
-
-    // Enable two-sided lighting.
-    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
-    // Enable local viewpoint.
-    glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+		// Enable two-sided lighting.
+		glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
+		// Enable local viewpoint.
+		glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+	#endif
 
 	// clearing color
 	glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -384,7 +311,13 @@ void init() {
 	glLoadIdentity();
 	gluLookAt(0.0, 70.0, 50.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
-	//glutTimerFunc(1000/60, rotatePlanetAngle, angle);
+	#ifdef LIGHTING
+		glShadeModel(GL_SMOOTH);
+	#endif
+
+	#ifdef ANIMATION
+		glutTimerFunc(1000/60, rotatePlanetAngle, angle);
+	#endif
 }
 
 int main(int argc, char** argv) {
